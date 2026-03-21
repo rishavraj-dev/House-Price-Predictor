c97286c86bf45fbe20231bba0ede1c25778882da
Rishav Raj <rishavraj.dev@outlook.com>
2026-03-22 00:18:49 +0530

    Add training script for House Price Predictor model

---
+++ train.py
@@ -0,0 +1,34 @@
+from sklearn.pipeline import Pipeline
+
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.compose import ColumnTransformer
+from sklearn.linear_model import LinearRegression    
+import pickle
+
+import pandas
+
+data=pandas.read_csv('dataset/dataset.csv')
+
+features=['Location', 'Status', 'Floor', 'Furnishing', 'Facing']
+numbers=['BHK', 'Area', 'Is_Resale', 'Bathrooms', 'Balcony']
+input_features =data.drop('Price',axis=1)
+predicted_price=data['Price']
+
+processer =ColumnTransformer(
+    transformers=[
+    ('num', 'passthrough', numbers),
+    ('cat', OneHotEncoder(handle_unknown='ignore'), features)
+])
+final   = Pipeline(steps=[
+         ('preprocessor', processer),
+        ('model', LinearRegression())
+])
+
+
+
+
+final.fit(input_features, predicted_price)
+
+with open('model/model.pkl', 'wb') as file:
+      pickle.dump(final, file)
+print("model is now saved in model folder ")
